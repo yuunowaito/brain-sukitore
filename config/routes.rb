117413cb2032526d494b2adf55dc32ef79d7c12c
yuunowaito <nowaito45@gmail.com>
2026-03-14 01:20:07 +0900

    ログイン・ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
