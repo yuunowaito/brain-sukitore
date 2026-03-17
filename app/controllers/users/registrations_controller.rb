198d9af053817f1ee06c6e3323f7147dc798f222
yuunowaito <nowaito45@gmail.com>
2026-03-18 00:32:23 +0900

    テスト修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [ :create ]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -43,7 +43,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: [:name]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ profile_attributes: [ :name ] ])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_one :profile
   accepts_nested_attributes_for :profile
   delegate :name, to: :profile, allow_nil: true
-  
-
 end
