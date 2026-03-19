71b227f2c65dd74a23cd609573eb44fc3fe86868
yuunowaito <nowaito45@gmail.com>
2026-03-19 11:28:32 +0900

    パスワードリセット機能実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,16 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.default_url_options = { host: "brain-sukitore.onrender.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "brain-sukitore.onrender.com",
+    user_name: ENV["MAILER_SENDER"],
+    password: ENV["MAILER_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 end
