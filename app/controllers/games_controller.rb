4277435b670d004d80988be954b0be32d8a29368
yuunowaito <nowaito45@gmail.com>
2026-03-30 23:20:23 +0900

    GamesControllerの作成と説明画面・問題画面の表示を実装

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+  def index
+    @game_type = GameType.find_by!(name: 'hiragana_calc')
+  end
+
+  def show
+    @question = QuestionGenerator.generate
+  end
+end
