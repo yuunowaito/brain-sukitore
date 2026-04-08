9b72a5da1b9a43463f57fa80e1db866022e5ef9f
yuunowaito <nowaito45@gmail.com>
2026-04-09 07:45:55 +0900

    認証画面のUI改善とアカウント削除時のプロフィール連動削除を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :profile
+  has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
   delegate :name, to: :profile, allow_nil: true
 end
