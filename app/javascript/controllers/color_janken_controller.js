bf4f3b178fc007552382ac55f505cfdb836657d6
yuunowaito <nowaito45@gmail.com>
2026-05-14 08:57:09 +0900

    fix: 本番環境で色じゃんけんの画像パスを修正

--- app/javascript/controllers/color_janken_controller.js
+++ app/javascript/controllers/color_janken_controller.js
@@ -3,16 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="color-janken"
 export default class extends Controller {
   static targets = ["question"]
-  static values = { answer: String }
+  static values = { answer: String, images: Object  }
 
   gameQuestionUpdated(event) {
     const question = event.detail.question
-    const color = question.color
-    const hand = question.hand
-
-    this.questionTarget.src = `/assets/${hand}_${color}.svg`
+    const key = `${question.hand}_${question.color}`
+    this.questionTarget.src = this.imagesValue[key]
     this.questionTarget.previousElementSibling.textContent =
-      color === "blue" ? "勝つ手を出せ！" : "負ける手を出せ！"
+      question.color === "blue" ? "勝つ手を出せ！" : "負ける手を出せ！"
     this.answerValue = question.answer
   }
 }
