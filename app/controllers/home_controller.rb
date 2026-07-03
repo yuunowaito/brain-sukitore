264574132b573b0ff894b491004335056757d3fe
yuunowaito <nowaito45@gmail.com>
2026-07-03 14:16:49 +0900

    N+1問題を解消: チャートデータ生成でスコアを一括取得・bullet導入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,16 +6,16 @@ class HomeController < ApplicationController
     @today_game = GameType.find_by(name: "hiragana_calc")
 
     if user_signed_in?
+      # 全スコアを1回だけ取得（ループの外）
+      all_scores = current_user.scores.order(played_on: :asc).to_a
+
       @chart_data = GameType.all.each_with_object({}) do |gt, hash|
-        scores = current_user.scores
-                            .where(game_type: gt)
-                            .order(played_on: :asc)
-                            .last(10)
+        # DBに行かず、取得済みのall_scoresから選ぶだけ
+        scores = all_scores.select { |s| s.game_type_id == gt.id }.last(10)
+
         hash[gt.display_name] = {
           labels: scores.map { |s| s.played_on.strftime("%-m/%-d") },
-          datasets: [ {
-            data: scores.map(&:score)
-          } ]
+          datasets: [ { data: scores.map(&:score) } ]
         }
       end
     end
